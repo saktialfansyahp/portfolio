a20b6cf28d19ec49ff5712a039e530a1afff7142
saktialfansyahp <saktialfansyahp@gmail.com>
2023-08-11 16:45:44 +0700

    <refactor> change navbar position

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,5 @@
 <template>
   <v-app-bar app color="black" dark flat class="px-12">
-    <v-btn>
-      <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon>Alfan
-    </v-btn>
-    <v-spacer></v-spacer>
     <v-btn text @click="scroll('home')" class="text-tellow">Home</v-btn>
     <v-btn text @click="scroll('about')" class="text-tellow">About</v-btn>
     <v-btn text @click="scroll('portfolio')" class="text-tellow"
@@ -13,6 +9,10 @@
     <!-- <v-btn text @click="scroll('page')" class="text-tellow">Pages</v-btn> -->
     <!-- <v-btn text @click="scroll('biog')" class="text-tellow">Blog</v-btn> -->
     <v-btn text @click="scroll('contact')" class="text-tellow">Contact</v-btn>
+    <v-spacer></v-spacer>
+    <v-btn>
+      <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon>Alfan
+    </v-btn>
   </v-app-bar>
 </template>
 
